1c2d89f11addb79c8296841d287b5909a8facac8
”Jakob <jako500n@stud.kea.dk>
2022-09-27 09:47:54 +0200

    cookieboks

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+// COOKIES
+
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieBoks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+  cookieBoks.classList.add("hidden");
+  sessionStorage.setItem("fjerncookieboks", true);
+}
+
+if (sessionStorage.getItem("fjerncookieboks")) {
+  cookieBoks.classList.add("hidden");
+}
